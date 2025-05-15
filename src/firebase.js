5f6b51e44b142f3ca96dd1b9e8ee7ee6198bbc35
Sophie <038sophienk@gmail.com>
2025-05-15 10:08:19 +0300

    added securty

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "sophielogin-e58ad.firebaseapp.com",
-  projectId: "sophielogin-e58ad",
-  appId: "AIzaSyDhoAQ2GZZ7aYSeraHFXePWEJipZe2eePQ",
-};
+// const firebaseConfig = {
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "sophielogin-e58ad.firebaseapp.com",
+//   projectId: "sophielogin-e58ad",
+//   appId: "AIzaSyDhoAQ2GZZ7aYSeraHFXePWEJipZe2eePQ",
+// };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-const facebookProvider = new FacebookAuthProvider();
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const googleProvider = new GoogleAuthProvider();
+// const facebookProvider = new FacebookAuthProvider();
 
-export { auth, googleProvider, facebookProvider };
+// export { auth, googleProvider, facebookProvider };
